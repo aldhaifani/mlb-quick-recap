74fca6fceecbd5fff4698d22041517235ea4b2b0
Tareq <tareqaldhaifani@gmail.com>
2025-02-02 09:38:25 +0400

    Downgraded tailwindcss and stup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css";
+import './App.css'
+
+import { Button } from './components/ui/button'
 
 function App() {
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline">Button</Button>
     </>
   );
 }
 
-export default App;
+export default App
